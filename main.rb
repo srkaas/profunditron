a2d3babed19a7514dfeaa5ea92fe7950498b902e
Steven Kaas <steven0461@gmail.com>
2014-03-06 01:09:46 -0800

    First commit. Includes generator and data file.

---
+++ main.rb
@@ -0,0 +1,41 @@
+# A program that randomly generates wise statements, implementing what I think amounts to a context-free grammar.
+# With a different data file, it could be made to generate statements that aren't wise.
+# TO DO: consider fixing how it handles capitalization.
+
+require 'yaml'
+DATA = YAML.load_file('data.yaml')
+MAX_EXPANSION_LEVEL = 100 # Prevent accidental infinite loops.
+
+# Nugget of wisdom.
+class Wisdom < String
+  def initialize(initial_string)
+    super(initial_string)
+    expand_fully
+  end
+
+  # Are all the bracketed terms expanded?
+  def expanded?
+    !include?('[') && !include?(']')
+  end
+
+  # Expand all the bracketed terms once, replacing them by a random choice from the corresponding list in the data file.
+  def expand
+    gsub!(/\[(.*?)\]/) { |term| DATA[$1].sample }
+  end
+
+  # Expand until all the brackets are gone.
+  def expand_fully
+    expansion_level = 0
+    while !expanded? && expansion_level < MAX_EXPANSION_LEVEL
+      expansion_level += 1
+      expand
+    end
+  end
+end
+
+# Generate a nugget of wisdom.
+def test_me
+  puts Wisdom.new('[Initial]')
+end
+
+test_me
